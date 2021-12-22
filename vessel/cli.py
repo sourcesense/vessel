59f8d0a451b6d1e0593bd0ce39bd3bad9a3372af
niko.usai <niko.usai@sourcesense.com>
2021-12-22 17:03:20 +0100

    added pipeline

--- vessel/cli.py
+++ vessel/cli.py
@@ -28,7 +28,7 @@ def single(resource, tools, registries):
 
 @cli.command()
 @click.option('-n', '--namespaces', envvar='NAMESPACES', default=None, type=click.STRING, help="Namespaces to watch separated by commas [default]")
-@click.option('-t', '--tools', envvar='TASKS', default="linter", type=click.STRING, help="Tools to run separated by commas [default]")
+@click.option('-t', '--tools', envvar='TOOLS', default="linter,trivy", type=click.STRING, help="Tools to run separated by commas [default]")
 @click.option('-d', '--data', default='./data.db', envvar='DATA', type=click.Path( dir_okay=False, writable=True), help="data file [./data.db]")
 @click.option('--k8s-url', envvar='K8S_URL', type=click.STRING, help="Kubernetes url, if null service account will be used [null]")
 @click.option('--k8s-token', envvar='K8S_TOKEN', type=click.STRING, help="Kubernetes authentication token [null]")
@@ -39,7 +39,7 @@ def server(namespaces, tools, data, k8s_url, k8s_token, registries, verbose):
     click.echo(f"Running for namespaces: {namespaces}")
     ctx = Context(registries)
     manager = ToolsManager(tools, ctx)
-    logging.basicConfig(format='%(asctime)s[%(module)-.8s][%(threadName)s][%(levelname)-5s]: %(message)s', datefmt='%m/%d/%Y %I:%M:%S', level=logging.DEBUG if verbose else logging.INFO)
+    logging.basicConfig(format='%(asctime)s [%(module).8s][%(levelname)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S', level=logging.DEBUG if verbose else logging.INFO)
 
     logging.info("Starting Vessel")
     start(data, manager, namespaces, k8s_url, k8s_token)
